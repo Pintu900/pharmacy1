22b4dfd630d17f77abc95d433efcf71815ba5a10
pintu900 <shahpintu900@gmail.com>
2021-07-17 21:23:20 +0530

    A values showing

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,8 +1,11 @@
 import React,{useState,useEffect} from 'react';
-
+let salad = [];
 function Fsearch(props){
+    
+   
     const [value,setValue]=useState('');
     const [result,setResult]=useState(['']);
+
     useEffect(()=>{
         if(value.length>0){
         fetch('https://pharmacy-6bcb1.firebaseio.com/record.json')
@@ -23,23 +26,45 @@ function Fsearch(props){
              console.log(error);
          })
         }else{
-            setResult([])
+         fetch('https://pharmacy-6bcb1.firebaseio.com/record.json')
+         .then(response => response.json())
+         .then(data => {
+             setResult([]);
+            // let searchQuery=value.toLowerCase();
+             for(const key in data){
+                 let med=data[key].name.toLowerCase();
+                 if(med.slice(0,1).indexOf('a') !== -1){
+                     setResult(prevResult=>{
+                         return [...prevResult,data[key]]
+                     })
+                 }
+             }
+             
+         }).catch(error => {
+             console.log(error);
+         })
         }
     },[value])
+
+     result.sort((a, b)=>{
+      var textA = a.name.toUpperCase();
+      var textB = b.name.toUpperCase();
+      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+     })
 
     return (
         
             <div className="container">
            <center> <h3><b><p className="text-success"></p></b></h3></center>
            <br></br><br></br>
-           <div className="container-sm">
+           <div className="container-sm sticky-top">
           <center><input type="text" className="form-control" placeholder="Search Medicine here"
             onChange={(event)=> setValue(event.target.value)}
             value={value}
             /></center>  </div>
             <div className="SearchBack">
                 <div className="SearchEntry">
-                <table class="table table-striped">
+                <table className="table table-striped mb-5">
   <thead>
     <tr>
       <th scope="col">#</th>
@@ -49,20 +74,25 @@ function Fsearch(props){
     </tr>
   </thead>
   <tbody>
-  {result.map((result,index) => (
+  {result?.map((salad,index) => (
                         <tr>
       <th scope="row">{index+1}</th>
-      <td>{result.name}</td>
-      <td>{result.box}</td>
-      <td>{result.price}</td>
+      <td>{salad.name}</td>
+      <td>{salad.box}</td>
+      <td>{salad.price}</td>
     </tr>        
-                ))}
+  ))}
   </tbody>
 </table>
                     
                 </div>
             </div>
+            <footer class="container fixed-bottom bg-white">
+    <p class="float-end"><a href="#">Back to top</a></p>
+    <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </footer>
             </div> 
+            
         
     )
 }
